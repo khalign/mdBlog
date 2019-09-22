75cfac1d60cafef78540bf0865be187f6b55cabe
khalig-n <khalig.novruzli@gmail.com>
2019-09-22 15:23:59 +0300

    Created template for md posts, added 2 md examples and installed styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,63 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const PostLink = styled(Link)`
+  text-decoration: none;
+`
+const PostTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
 
-export default IndexPage
+export default ({ data }) => {
+  const { nodes } = data.allMarkdownRemark
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+
+      {nodes.map(node => {
+        const md = node.frontmatter
+        return (
+          <div key={node.id}>
+            <PostLink to={node.fields.slug}>
+              <PostTitle>
+                {md.title} - {md.date}
+              </PostTitle>
+            </PostLink>
+
+            <p>{node.excerpt}</p>
+          </div>
+        )
+      })}
+
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      nodes {
+        excerpt
+        frontmatter {
+          date
+          description
+          title
+        }
+        id
+        fields {
+          slug
+        }
+      }
+    }
+  }
+`
